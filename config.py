1d458ffdbd1f5ae7ab082142ff568647c3548486
Zainul Khan <zk863054@gmail.com>
2025-12-03 22:50:16 +0530

    all files are added for the fast api project

--- config.py
+++ config.py
@@ -1,11 +1,19 @@
 
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+import certifi
 
-uri = "mongodb+srv://zk863054_db_user1:08xFKui3RDNscJyT@cluster0.dcs5shz.mongodb.net/?appName=Cluster0"
+uri = "mongodb+srv://zk863054_db_user1:08xFKui3RDNscJyT@cluster0.dcs5shz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
-# Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
+# Create a new client and connect to the server with proper SSL configuration
+client = MongoClient(
+    uri, 
+    server_api=ServerApi('1'),
+    tlsCAFile=certifi.where(),
+    connectTimeoutMS=30000,
+    socketTimeoutMS=30000,
+    serverSelectionTimeoutMS=30000
+)
 
 db = client.incident_db
 
